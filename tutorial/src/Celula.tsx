f56f0a592ea861cc3228132c40805536888c0b46
Eduardo P de Sousa <edupsousa@gmail.com>
2022-10-17 15:19:50 -0300

    mudar valor da célula quando clicar

--- tutorial/src/Celula.tsx
+++ tutorial/src/Celula.tsx
@@ -1,8 +1,22 @@
+import { useState } from "react";
+
+type ValorCelula = 0 | 1 | 2;
+
 type Props = {
-  valor: 0 | 1 | 2;
+  valor: ValorCelula;
 };
 
-export default function Celula({ valor }: Props) {
+export default function Celula(props: Props) {
+  const [valor, setValor] = useState(props.valor);
+
   const texto = valor === 1 ? "X" : valor === 2 ? "O" : "";
-  return <div className="celula">{texto}</div>;
+  const alterarValor = () => {
+    setValor(((valor + 1) % 3) as ValorCelula);
+  };
+
+  return (
+    <div className="celula" onClick={alterarValor}>
+      {texto}
+    </div>
+  );
 }
